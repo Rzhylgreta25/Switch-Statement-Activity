cae082d249a367113213e86b1f3c5b40063266f4
Your Name <rzhyl05@gmail.com>
2024-10-10 21:12:40 +0800

    your commit message

--- taskCalendar.js
+++ taskCalendar.js
@@ -1,34 +1,28 @@
-let day = "sunday" ;
-console.log("Enter the day of the week:");
-switch (day) {
-    case "monday":
-        console.log("it is monday");
-        console.log("your task for monday: Group meeting at 9:00am");
-        break;
-    case "Tuesday":
-        console.log("it is tuesday");
-        console.log("your task for tuesday: Work on project");
-        break;
-    case "wednesday":
-        console.log("it is wednesday");
-        console.log("you task for wednesday: go to work at 8;30am");
-        break;
-    case "thursday":
-        console.log("it is thursday");
-        console.log("your task for thursday: go to school at 7:30am");
-        break;
-    case "friday":
-        console.log("it is friday");
-        console.log("your task for friday: submit all projects");
-        break;
-    case "saturday":
-        console.log("it is saturday");
-        console.log("your task for saturday: rest");
-        break;
-    case "sunday":
-        console.log("it is sunday");
-        console.log("your task for sunday: relaxition day");
-        break;
-        default:
-    console.log("please enter the valid week");
+function Calender() {
+    let Calender = prompt("Enter the day of the week.");
+    switch (Calender) {
+        case "Monday":
+            alert("your task for Monday: Group meeting at 9:00am");
+            break;
+        case "Tuesday":
+            alert("your task for Tuesday: Work on Project");
+            break;
+        case "Wednesday":
+            alert("your task for Wednesday: Clean the house");
+            break;
+        case "Thursday":
+            alert("your task for Thursday: give the dog a bath");
+            break;
+        case "Friday":
+            alert("your task for Friday:submit all project");
+            break;
+        case "Saterday":
+            alert("your task for Saterday: go out with friends");
+            break;
+        case "Sunday":
+            alert("your task for Sunday: rest and relax");
+            break;
+            default:
+            alert("invalid")
+    }
 }
